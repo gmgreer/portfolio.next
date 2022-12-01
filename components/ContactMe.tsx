aad533c5b8040a142dccbe47d0737c2f74d4927a
gmgreer <beauski8@gmail.com>
2022-12-01 14:56:23 -0800

    fixed two components mobile problems

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -17,7 +17,7 @@ const { register, handleSubmit, } = useForm<Inputs>();
 
   return (
     <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-        <h3 className='absolute top-16 first-letter:uppercase tracking-[20px] text-gray-500 text-2xl'>Contact
+        <h3 className='invisible sm:visible absolute top-16 first-letter:uppercase tracking-[20px] text-gray-500 text-2xl'>Contact
        </h3>
         <div className='flex flex-col space-y-10'>
             <h4 className='text-4xl font-semibold text-center'> <span className='decoration-[orange]/50 underline'>Lets Talk</span> </h4>
